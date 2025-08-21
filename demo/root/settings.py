62a652225fe2c0223ab048965035df3c6c8a633f
ns-ey <nicolo.scarsi@it.ey.com>
2025-08-21 13:40:36 +0200

    aggiunto fluent

--- demo/root/settings.py
+++ demo/root/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'fluent',
     'django_scim',
     'oauth2_provider',
     'app',
