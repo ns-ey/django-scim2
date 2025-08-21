321b2a6224a15dde3442e42e91b7004b86f5da45
ns-ey <nicolo.scarsi@it.ey.com>
2025-08-21 13:59:55 +0200

    fix

--- demo/root/settings.py
+++ demo/root/settings.py
@@ -13,8 +13,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
-#from fluent import sender
-from fluent.sender import FluentSender
+from fluent import sender
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
